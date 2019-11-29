89736a82fc6cb48b833c59f2552018a1cf24e751
Khairul Anam <khairul@Kabirs-Mac-mini.local>
2019-11-29 15:21:59 +0600

    First commit

---
+++ app/alloy.js
@@ -0,0 +1,5 @@
+import test from 'com.bd.test';
+Ti.API.info('module is => ${test}');
+Ti.API.info('module example() method returns => ${test.example()}');
+Ti.API.info('module exampleProp is => ${test.exampleProp}');
+test.exampleProp = "This is a test value";
